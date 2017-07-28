49ec10bfd56b881512e6ebb526bbecc2ce1ac694
Christelle <eloundou.c@gmail.com>
2017-07-28 12:37:33 +0200

    Routes ok

---
+++ app/controllers/corsaires_controller.rb
@@ -0,0 +1,13 @@
+class CorsairesController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
 
+  get 'corsaires', to: 'corsaires#index'
+
+  get 'corsaires/new', to: 'corsaires#new'
+
+  get 'corsaires/show', to: 'corsaires#show'
+
+  get 'corsaires/edit', to: 'corsaires#edit'
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
